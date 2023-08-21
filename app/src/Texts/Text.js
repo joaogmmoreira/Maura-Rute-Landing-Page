6d76b85b011b08aa3d3c86bfb3d3362bf436fd49
João Moreira <jgmoreira1809@gmail.com>
2023-08-21 20:53:54 -0300

    Create Text.js

---
+++ app/src/Texts/Text.js
@@ -0,0 +1,137 @@
+// Academia de Mulheres Inspire é um projeto que Deus colocou no meu coração no final de 2019.
+// Na época, já liderava mulheres, mas sentia a necessidade de como líder,
+// desenvolver minha equipe nas três dimensões: espiritual, física e emocional.
+// Percebia que precisávamos crescer e desenvolver uma perspectiva mais ousada e corajosa
+// em relação a liderança cristã.
+// As pressões sobre as líderes por vezes as tornava impotentes diante de tantos
+// desafios que as cercavam na vida pessoal, familiar, no profissional e ministerial.
+// Elas precisavam de ferramentas; mais do que isso, elas estavam sedentas por apoio,
+// pastoreio e cuidado.
+// Precisavam desenvolver uma liderança saudável, mas não sabiam por onde começar.
+// Então, começamos a nos reunir, trocar informações e experiências. Deus foi me dando
+// estratégias para torná-las capazes de conhecerem a si mesmas e as suas lideradas.
+// Começamos juntas a administrar melhor os nossos dons, talentos, nosso tempo e as
+// demandas que precisávamos dar conta.
+// Foi assim que surgiu a ACADEMIA DE MULHERES INSPIRE.
+// Como um espaço de crescimento mútuo e de muita construção em conjunto.
+// Aos poucos, o desânimo, foi dando lugar a motivação; a indisciplina foi
+// anulada pela disciplina e pela boa administração do tempo; a inexperiência foi
+// dando lugar a troca e a bons relacionamentos.
+
+// APRESENTAÇÃO PESSOAL
+// Filha amada do Pai, convertida e chamada desde a adolescência;
+// Filha, esposa e mãe
+// Formada em Educação Religiosa e Pedagogia pelo
+// Seminário Teológico Batista do Sul do Brasil (1996);
+// Formada em Serviço Social pela Universidade do Grande Rio - UNIGRANRIO (2015)
+// Pós-Graduada em Terapia Sistêmica Individual, casal e Família pelo Instituto Logos (2021)
+// Articulista das Revistas Visão Missionária, Aventura Real e Manancial da UFMBB
+// Líder das Esposas de pastores da Convenção Batista Carioca RJ (2019)
+// Casada com o pr. José Paulo Moura Antunes desde 1995
+// Membro da Primeira Igreja Batista em Madureira -
+// Rio de janeiro - onde seu esposo é o pastor titular.
+
+// O PROJETO DE MENTORIA E DISCIPULADO
+
+// 1.OBJETIVO
+// O programa propõe o discipulado e o acompanhamento da mulher
+// líder nos seguintes aspectos: espiritual, físico e emocional; por meio do fornecimento de
+// ferramentas e recursos para o crescimento espiritual, desenvolvimento da inteligência
+// emocional e consciência do cuidado com o corpo e saúde.
+
+// 2.JUSTIFICATIVA
+// O programa de discipulado e mentoria para liderança feminina,
+// será uma resposta a lacuna que temos no que diz respeito ao preparo integral da líder.
+// Tem como estratégia de atuação principal aliar teoria a pratica, à medida que as mentoradas
+// também terão oportunidade de discipular outras mulheres.
+
+// ESSA MENTORIA É PARA VOCÊ QUE:
+
+// NÃO CONSEGUE TREINAR UMA PESSOA PARA SUBSTITUIR
+// TEM DIFICULDADE COM A LIDERANCA DA IGREJA
+// ACHA QUE MINISTERIO E MENOSPREZADO
+// ESQUECE DA IMPORTANTECIA DO MINISTERIO DAINTE DE DEUS
+// ESTÁ QUASE DESISTINDO DO MINISTERIO
+// TEM DIFICULDADE DE IDENTIFICAR NOVOS LIDERES
+// SE SENTE SOZINHA NO MINISTÉRIO
+// NÃO SABE DELEGAR E ACUMULA FUNÇÕES
+// TEM DIFICULDADE DE PLANEJAR
+// NÃO CONSEGUE DAR PROSSEGUIMENTOS AO PLANEJAMENTO ANUAL
+// TÁ TÃO DESANIMADA QUE FAZ TUDO DE QUALQUER JEITO
+// NÃO TEM MAIS UMA VIDA DE ORAÇÃO e NÃO LE mais A BIBLIA e NÃO TEM TEMPO COM DEUS
+// SO FOCA NO ATIVISMO
+// Por causa da sobrecarga está com o LAR DESAJUSTADO
+
+// EU ENSINO MULHERES A:
+
+// ESTAR AJUSTADA NO MINISTERIO E NO LAR
+// SE ORGANIZAR PARA TER MAIS TEMPO COM A FAMIALIA
+// TER UM DIALOGO ESCLARECIDO COM A IGREJA
+// FORMAR OUTRAS LIDERES
+// COLOCAR AS PESSOAS CERTAS NO LUGAR CERTO
+
+// E QUEM MULHERES SÃO ESSAS?
+
+// PUBLICO ALVO - para quem é o treinamento de liderança cristã feminina?
+// - pastoras
+// - esposa de pastor
+// - lideres de mulheres (ministério de mulheres, MCM, jovens, MR)
+// - lideres de crianças
+// - Profissionais em qualquer área de atuação
+
+// 3.FORMATO:
+// Os encontros acontecerão quinzenalmente de forma online
+// AS TERCAS FEIRAS AS 20H (Google Meet), com duração de 1 hora e meia a 2 horas.
+
+// Os ENCONTRO ficarão GRAVADOS E A DISPOSIÇÃO DAS ALUNAS inscritas.
+
+// TODAS AS AULAS SERÃO AO VIVO
+
+// 4.DURAÇÃO:
+// Programa se dará em 6 meses - totalizando 12 encontros.
+
+// 5.CONTEÚDO:
+// Os conteúdos serão apresentados em formato de módulos
+// para melhor compreensão e absorção dos mesmos.
+
+// INTRODUÇÃO (1 encontro)
+// - Liderança Feminina – Mulher em Posição de Destaque
+
+// MODULO 1 – QUEM SOU EU? (4 encontros)
+// - Identidade e autoconhecimento
+// - Identidade ferida x identidade curada
+// - Crenças limitantes
+// - Entendendo o processo entre a dor e a cura
+// - Entendendo o temperamento para gerenciar as emoções
+// - Entendendo a personalidade para gerenciar as emoções
+// - Construção da autoimagem
+// - Desenvolvimento da autoestima
+
+// MODULO 2 – VIDA CRISTÃ (4 encontros)
+// - Reafirmando o meu Propósito (não ora mais, não lê a Bíblia, qual é o seu propósito em Deus)
+// - Desenvolvimento do Fruto do Espírito
+// (domínio próprio, papel que não são nossos, afeta a família, com os outros líderes na igreja)
+// - Desenvolvimento dos Dons (líderes no lugar errado)
+// - Desenvolvimento das Linguagens do Amor (como está o seu relacionamento com a família)
+// - Batalha Espiritual (caso de doença dentro de casa e não sabe que é uma luta espiritual)
+// - Missões, chamado e vocação (quando a gente perde o
+// proposito, recebe o chamado mas não está focado)
+
+// MODULO 3 – LIDERANÇA (3 encontros)
+// - Desenvolvimento da inteligência emocional
+// - Discipulado Comportamental
+// - Discipulando outras mulheres
+
+// 6.PERFIL DE SAÍDA:
+
+// Ao final do período, a líder será capaz de:
+
+// - Conhecer a si mesma; curar feridas emocionais; entender os temperamentos e
+// personalidades para gerenciar suas emoções e interromper valores nocivos e
+// reforçar bons valores.
+
+// - Confirmar seu propósito, missão e chamado; aprender a desenvolver seus Dons,
+// Fruto do Espírito e Linguagem do amor; estar firme diante das batalhas espirituais.
+
+// - Desenvolver a inteligência emocional para liderança; entender o comportamento
+// da discipulanda para conseguir extrair o melhor que existe nela.
